01271c8566dbac9bf2c0c1d1793d2bcb6e5b0e96
MayurKh007 <mayurkhobragade@gmail.com>
2022-02-03 21:27:19 +0530

    this is my second commit

---
+++ src/main/java/JiraTestApis/JiraTest.java
@@ -0,0 +1,40 @@
+package JiraTestApis;
+
+import io.restassured.RestAssured;
+import io.restassured.filter.session.SessionFilter;
+import static io.restassured.RestAssured.*;
+
+import java.io.File;
+
+public class JiraTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		RestAssured.baseURI = "http://localhost:8080/";
+
+		SessionFilter session = new SessionFilter();
+
+		String response = given().log().all().header("content-type", "application/json")
+				.body("{ \"username\": \"Mayur\", \"password\": \"Mayur123\" }").log().all().filter(session).when()
+				.post("rest/auth/1/session").then().log().all().assertThat().statusCode(200).extract().response()
+				.asString();
+
+		// Add Comment
+
+		given().pathParam("key", "10100").log().all().header("content-type", "application/json")
+				.body("{\r\n" + "    \"body\": \"This is my first comment via automation.\",\r\n"
+						+ "    \"visibility\": {\r\n" + "        \"type\": \"role\",\r\n"
+						+ "        \"value\": \"Administrators\"\r\n" + "    }\r\n" + "}")
+				.filter(session).when().post("rest/api/2/issue/{key}/comment").then().log().all().assertThat()
+				.statusCode(201);
+
+		// Add attachment
+
+		given().header("X-Atlassian-Token", "no-check").filter(session).pathParam("key", "10100")
+				.header("Content-Type", "multipart/form-data").multiPart("file", new File("Attachment.txt")).when()
+				.post("rest/api/2/issue/{key}/attachments").then().log().all().assertThat().statusCode(200);
+
+	}
+
+}
